27950682c6e4276d60996612a0b71d06e7f273f9
Angelo Polotto <angelo.polotto@tcs.com>
2021-01-08 18:35:30 -0300

    xamarin start app

---
+++ Cryptollet/Cryptollet/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cryptollet
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
