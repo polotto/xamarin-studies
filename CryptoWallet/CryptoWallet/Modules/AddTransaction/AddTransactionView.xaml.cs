8b186ada71ff3950fbe0e336efe9984814a4a9ca
Angelo Polotto <angelo.polotto@tcs.com>
2021-01-12 16:30:04 -0300

    Section 4: Shell Navigation

--- CryptoWallet/CryptoWallet/Modules/AddTransaction/AddTransactionView.xaml.cs
+++ CryptoWallet/CryptoWallet/Modules/AddTransaction/AddTransactionView.xaml.cs
@@ -5,22 +5,12 @@ using Xamarin.Forms;
 
 namespace CryptoWallet.Modules.AddTransaction
 {
-    [QueryProperty("Id", "id")]
     public partial class AddTransactionView : ContentPage
     {
         public AddTransactionView()
         {
             InitializeComponent();
             BindingContext = App.Container.Resolve<AddTransactionViewModel>();
-        }
-
-        private string _id;
-        public string Id
-        {
-            set
-            {
-                _id = Uri.UnescapeDataString(value);
-            }
         }
     }
 }
