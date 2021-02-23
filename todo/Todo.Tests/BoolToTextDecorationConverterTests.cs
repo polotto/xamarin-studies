16fd16bdd1da5432901781b808cace1e24ae6290
Angelo Polotto <angelo.polotto@metait.ca>
2021-02-23 15:24:27 -0300

    - Section 3: Unit Testing - Remove Crashes From Your App

---
+++ todo/Todo.Tests/BoolToTextDecorationConverterTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+using todo;
+using Xamarin.Forms;
+
+namespace Todo.Tests
+{
+    public class BoolToTextDecorationConverterTests
+    {
+        [Fact]
+        public void Convert_returns_strikethrough_when_item_is_completed()
+        {
+            // arrange
+            var converter = new BoolToTextDecorationConverter();
+
+            // act
+            var result = converter.Convert(true, null, null, null);
+
+            // assert
+            Assert.Equal(TextDecorations.Strikethrough, (TextDecorations)result);
+        }
+
+        [Fact]
+        public void Convert_returns_none_when_item_is_not_completed()
+        {
+            // arrange
+            var converter = new BoolToTextDecorationConverter();
+
+            // act
+            var result = converter.Convert(false, null, null, null);
+
+            // assert
+            Assert.Equal(TextDecorations.None, (TextDecorations)result);
+        }
+    }
+}
---
+++ todo/Todo.Tests/TodoViewModelTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using todo;
+using Xunit;
+
+namespace Todo.Tests
+{
+    public class TodoViewModelTests
+    {
+        [Fact]
+        public void ViewModel_populates_items_correctly()
+        {
+            TodoViewModel viewModel = new TodoViewModel();
+
+            Assert.Equal(5, viewModel.Items.Count);
+        }
+
+        [Fact]
+        public void AddItemCommand_adds_new_item_to_the_list()
+        {
+            TodoViewModel viewModel = new TodoViewModel();
+
+            viewModel.AddItemCommand.Execute(null);
+
+            Assert.Equal(6, viewModel.Items.Count);
+            Assert.Equal("Todo Item 6", viewModel.Items.Last().Name);
+        }
+
+        [Fact]
+        public void MarkAsCompletedCommand_marks_item_as_completed_and_puts_it_at_the_end_of_the_list()
+        {
+            TodoViewModel viewModel = new TodoViewModel();
+
+            viewModel.MarkAsCompletedCommand.Execute(viewModel.Items.First());
+
+            Assert.True(viewModel.Items.Last().Completed);
+        }
+
+        [Fact]
+        public void MasrkAsCompletedCommand_updates_progress()
+        {
+            TodoViewModel viewModel = new TodoViewModel();
+
+            viewModel.MarkAsCompletedCommand.Execute(viewModel.Items.First());
+
+            Assert.Equal("Completed 1/5", viewModel.CompletedHeader);
+            Assert.Equal(0.2, viewModel.CompletedProgress);
+        }
+    }
+}
