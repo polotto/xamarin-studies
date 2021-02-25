f3e575748b7d7543a5ac08e5f45d10ddab4eb3a9
Angelo Polotto <angelo.polotto@metait.ca>
2021-02-25 17:08:12 -0300

    dialog updated with new features

--- Navigation/Navigation/Common/Dialog/DialogMessage.cs
+++ Navigation/Navigation/Common/Dialog/DialogMessage.cs
@@ -7,9 +7,21 @@ namespace Navigation
     public class DialogMessage : IDialogMessage
     {
         public async Task DisplayAlert(string title, string message, string cancel)
-        {
-            await Application.Current.MainPage.DisplayAlert(title, message, cancel);
-        }
+    {
+        await Application.Current.
+                MainPage.DisplayAlert(title, message, cancel);
+    }
+
+    public Task<string> DisplayPrompt(string title, string message)
+    {
+        return Application.Current
+                .MainPage.DisplayPromptAsync(title, message);
+    }
+
+    public Task<string> DisplayActionSheet(string title, string destruction, params string[] buttons)
+    {
+        return Application.Current.MainPage.DisplayActionSheet(title, "Cancel", destruction, buttons);
+    }
     }
 }
 
--- Navigation/Navigation/Common/Dialog/IDialogMessage.cs
+++ Navigation/Navigation/Common/Dialog/IDialogMessage.cs
@@ -6,6 +6,8 @@ namespace Navigation
     public interface IDialogMessage
     {
         Task DisplayAlert(string title, string message, string cancel);
+        Task<string> DisplayPrompt(string title, string message);
+        Task<string> DisplayActionSheet(string title, string destruction, params string[] buttons);
     }
 }
 
